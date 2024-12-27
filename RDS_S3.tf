917fbe599c32d2ef73955f3f46b8898e0781dd16
jeromejay09 <jeromejaygeronimo1@gmail.com>
2024-12-28 01:36:26 +0800

    Update RDS_S3.tf

--- RDS_S3.tf
+++ RDS_S3.tf
@@ -1,40 +1,45 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.20.1"
-    }
-  }
-
-  required_version = ">= 1.1.0"
-}
-
 provider "aws" {
-  region = "ap-southeast-1"  # Specify the region here
+  region = "ap-southeast-1" # Update to your desired region
 }
 
+# S3 Bucket
 resource "aws_s3_bucket" "pos_bucket" {
   bucket = "pos-system-bucket2"
+
+  tags = {
+    Name = "POS System Bucket"
+  }
+}
+
+resource "aws_s3_bucket_acl" "pos_bucket_acl" {
+  bucket = aws_s3_bucket.pos_bucket.id
   acl    = "private"
 }
 
-resource "aws_db_instance" "pos_db" {
+resource "aws_db_instance" "pos_system_db" {
   allocated_storage    = 20
+  storage_type         = "gp2"
+  instance_class       = "db.t3.micro"  # Correct parameter name is 'instance_class'
   engine               = "mysql"
   engine_version       = "8.0"
-  instance_class       = "db.t2.micro"
-  name                 = "posdb"
+  identifier           = "pos-system-db"
   username             = "admin"
   password             = "admin123"
-  parameter_group_name = "default.mysql8.0"
+  db_name              = "posdb"
+  skip_final_snapshot  = true
+  multi_az             = false
   publicly_accessible  = true
+
+  tags = {
+    Name = "POS System Database"
+  }
 }
 
-
+# Outputs
 output "rds_endpoint" {
-  value = aws_db_instance.pos_system_db.endpoint
+  value = aws_db_instance.pos_db.endpoint
 }
 
 output "s3_bucket_name" {
-  value = aws_s3_bucket.pos_system_bucket2.bucket
+  value = aws_s3_bucket.pos_bucket.bucket
 }
