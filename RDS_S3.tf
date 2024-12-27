bcc027fcbd47dcc8eda1e64676c7476b250ce556
jeromejay09 <jeromejaygeronimo1@gmail.com>
2024-12-28 02:05:02 +0800

    Update RDS_S3.tf

--- RDS_S3.tf
+++ RDS_S3.tf
@@ -11,15 +11,11 @@ resource "aws_s3_bucket" "pos_bucket" {
   }
 }
 
-resource "aws_s3_bucket_acl" "pos_bucket_acl" {
-  bucket = aws_s3_bucket.pos_bucket.id
-  acl    = "private"
-}
-
+# RDS Instance
 resource "aws_db_instance" "pos_system_db" {
   allocated_storage    = 20
   storage_type         = "gp2"
-  instance_class       = "db.t3.micro"  # Correct parameter name is 'instance_class'
+  instance_class       = "db.t3.micro"  # Correct parameter name
   engine               = "mysql"
   engine_version       = "8.0"
   identifier           = "pos-system-db"
